3698c58ab42a8e0755c47e130531cf1186c6285e
eliasinacio <0.elias.inacio@gmail.com>
2024-09-05 21:34:16 -0300

    start project

---
+++ index.js
@@ -0,0 +1,17 @@
+const a1 = document.getElementById('a1')
+const a2 = document.getElementById('a2')
+const a3 = document.getElementById('a3')
+const b1 = document.getElementById('b1')
+const b2 = document.getElementById('b2')
+const b3 = document.getElementById('b3')
+const c1 = document.getElementById('c1')
+const c2 = document.getElementById('c2')
+const c3 = document.getElementById('c3')
+
+function markX(element) {
+    element.innerHTML = '&#10060;'
+}
+
+function markO(element) {
+    element.innerHTML = '&#11093;'
+}
