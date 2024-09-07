6b6b3394c6cc700db6f50dd997f1b1443e04af6c
eliasinacio <0.elias.inacio@gmail.com>
2024-09-07 18:20:17 -0300

    adding listeners and updating styles

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const allPositions = document.getElementsByClassName('position')
+
 const a1 = document.getElementById('a1')
 const a2 = document.getElementById('a2')
 const a3 = document.getElementById('a3')
@@ -8,10 +10,58 @@ const c1 = document.getElementById('c1')
 const c2 = document.getElementById('c2')
 const c3 = document.getElementById('c3')
 
-function markX(element) {
-    element.innerHTML = '&#10060;'
+const optionX = document.getElementById('optionX')
+const optionO = document.getElementById('optionO')
+
+const options = [optionX, optionO]
+
+function clearGame() {
+    for (pos of allPositions) {
+        pos.innerHTML = ''
+    }
 }
 
-function markO(element) {
-    element.innerHTML = '&#11093;'
+function getOption() {
+    for (option of options) {
+        if (option.checked) return option
+    }
 }
+
+function clearOption() {
+    for (option of options) {
+        option.checked = false
+    }
+}
+
+function switchOption() {
+    if (optionX.checked) {
+        optionX.checked = false
+        optionO.checked = true
+    } else optionX.checked = true
+}
+
+function mark(element) {
+    if (getOption().value === 'x') {
+        element.innerHTML = '&#10060;'
+    } else {
+        element.innerHTML = '&#11093;'
+    }
+}
+
+function play() {
+    clearGame()
+    clearOption()
+
+    for (pos of allPositions) {
+        pos.addEventListener('click', function (event) {
+            if (getOption()) {
+                if (event.target.innerHTML === '') {
+                    mark(event.target)
+                    switchOption()
+                }    
+            }
+        })
+    }
+}
+
+play()
---
+++ readme.md
@@ -0,0 +1,22 @@
+- Jogo da velha multiplayer
+
+- Ao iniciar a partida, deve-se sortear um jogador para iniciar
+- O jogador deve selecionar a opção X ou O e selecionar o local para marcar
+- O jogo deve barrar um jogador de jogar ao mesmo tempo que outro
+- Ao ter um vencedor o jogo deve mostrar uma mensagem e, posteriormente, reiniciar
+- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+** proxima ideia de jogo: forca
