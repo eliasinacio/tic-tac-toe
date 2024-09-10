ad0245b57c334f887147ccb0d42c65d717daabda
eliasinacio <0.elias.inacio@gmail.com>
2024-09-09 21:07:23 -0300

    finishing local game

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const allPositions = document.getElementsByClassName('position')
 
+const a0 = document.getElementById('a0')
 const a1 = document.getElementById('a1')
 const a2 = document.getElementById('a2')
-const a3 = document.getElementById('a3')
+const b0 = document.getElementById('b0')
 const b1 = document.getElementById('b1')
 const b2 = document.getElementById('b2')
-const b3 = document.getElementById('b3')
+const c0 = document.getElementById('c0')
 const c1 = document.getElementById('c1')
 const c2 = document.getElementById('c2')
-const c3 = document.getElementById('c3')
+
+const m = [
+  [a0, a1, a2],
+  [b0, b1, b2],
+  [c0, c1, c2]
+]
 
 const optionX = document.getElementById('optionX')
 const optionO = document.getElementById('optionO')
@@ -48,6 +54,47 @@ function mark(element) {
   }
 }
 
+function checkLine(x, y, z) {
+  console.log(x, y, z)
+  return (
+    x.innerText && y.innerText && z.innerText &&
+    x.innerText === y.innerText &&
+    x.innerText === z.innerText &&
+    y.innerText === z.innerText
+  )
+}
+
+function checkHasWinner() {
+  const grid = document.getElementById('grid')
+  const winMessage = document.getElementById('winMessage')
+
+  if (checkLine(m[0][0], m[0][1], m[0][2])) {
+    grid.classList.add('h0')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[1][0], m[1][1], m[1][2])) {
+    grid.classList.add('h1')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[2][0], m[2][1], m[2][2])) {
+    grid.classList.add('h2')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[0][0], m[1][0], m[2][0])) {
+    grid.classList.add('v0')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[0][1], m[1][1], m[2][1])) {
+    grid.classList.add('v1')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[0][2], m[1][2], m[2][2])) {
+    grid.classList.add('v2')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[2][0], m[1][1], m[0][2])) {
+    grid.classList.add('d0')
+    winMessage.style.opacity = 100
+  } else if (checkLine(m[0][0], m[1][1], m[2][2])) {
+    grid.classList.add('d1')
+    winMessage.style.opacity = 100
+  }
+}
+
 function play() {
   clearGame()
   clearOption()
@@ -57,6 +104,7 @@ function play() {
       if (getOption()) {
         if (event.target.innerHTML === '') {
           mark(event.target)
+          checkHasWinner()
           switchOption()
         }
       }
--- readme.md
+++ readme.md
@@ -4,7 +4,9 @@
 - O jogador deve selecionar a opção X ou O e selecionar o local para marcar
 - O jogo deve barrar um jogador de jogar ao mesmo tempo que outro
 - Ao ter um vencedor o jogo deve mostrar uma mensagem e, posteriormente, reiniciar
-- 
+- Perguntar inicialmente se o usuário vai jogar localmente ou criar uma sala
+- Criar salas?
+-
 
 
 
