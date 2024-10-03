89e74477791ac5e3a362ce386c2481c67574659f
Elias Inácio <0.elias.inacio@gmail.com>
2024-10-03 02:52:55 +0000

    fixes and features: - adicionado server para o client puxar o jogo do servidor

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import http from 'http'
+import createGame from './public/game.js'
 
 const app = express()
 const server = http.createServer(app)
 
 app.use(express.static('public'))
 
+const game = createGame()
+
+game.start()
+
 server.listen(3000, () => {
-    console.log(`listening on port: 3000`)
+    console.log('listening on port: 3000')
 })
